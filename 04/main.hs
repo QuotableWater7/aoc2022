67e99fd9bda3457043aeee8bebfb45593bd297ac
JT <jt@usekojo.com>
2022-12-12 18:31:01 -0800

    part 2

--- 04/main.hs
+++ 04/main.hs
@@ -3,10 +3,15 @@ import Data.List.Split
 
 data Schedule = Schedule { start::Int, end::Int } deriving(Show)
 
-isOverlapping :: Schedule -> Schedule -> Bool
-isOverlapping s1 s2 = 
+isFullyOverlapping :: Schedule -> Schedule -> Bool
+isFullyOverlapping s1 s2 =
   (start(s1) <= start(s2) && end(s1) >= end(s2)) || 
   (start(s2) <= start(s1) && end(s2) >= end(s1))
+
+isPartiallyOverlapping :: Schedule -> Schedule -> Bool
+isPartiallyOverlapping s1 s2 = 
+  (start(s1) <= start(s2) &&  end(s1) >= start(s2)) ||
+  (start(s2) < start(s1) && end(s2) >= start(s1))
 
 getScheduleFromString :: String -> Schedule
 getScheduleFromString [] = error "gah"
@@ -20,10 +25,12 @@ main = do
 
   -- part 1
   let assignmentTuples = map (map getScheduleFromString . (splitOn ",")) (lines contents)
-  let answerPart1 = foldr (\[x, y] -> (+ if isOverlapping x y then 1 else 0)) 0 assignmentTuples
+  let answerPart1 = foldr (\[x, y] -> (+ if isFullyOverlapping x y then 1 else 0)) 0 assignmentTuples
   print answerPart1
 
   -- part 2
+  let answerPart2 = foldr (\[x, y] -> (+ if isPartiallyOverlapping x y then 1 else 0)) 0 assignmentTuples
+  print answerPart2
 
   -- tie up loose ends
   hClose handle
