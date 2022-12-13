2a8f78d61bdf88075161ce78e3e0b3fae6a675f7
JT <jt@usekojo.com>
2022-12-12 16:56:09 -0800

    simplify part 1

--- 04/main.hs
+++ 04/main.hs
@@ -8,28 +8,22 @@ isOverlapping s1 s2 =
   (start(s1) <= start(s2) && end(s1) >= end(s2)) || 
   (start(s2) <= start(s1) && end(s2) >= end(s1))
 
-
 getScheduleFromString :: String -> Schedule
 getScheduleFromString [] = error "gah"
 getScheduleFromString str = Schedule { start=read(x), end=read(y) }
   where
     [x,y] = splitOn "-" str
 
-breakLineByComma :: String -> [String]
-breakLineByComma str = splitOn "," str
-
-
--- breakIntoTuples :: String -> (String, String)
-
 main = do
   handle <- openFile "/Users/josephbowler/agora/aoc2022/04/input.txt" ReadMode
   contents <- hGetContents handle
 
   -- part 1
-  let assignmentTuples = map (map getScheduleFromString . breakLineByComma) (lines contents)
-  let isOverlappingList = map (\[x, y] -> isOverlapping x y) assignmentTuples
-  let overlappingTuples = filter id isOverlappingList
-  print $ length overlappingTuples
+  let assignmentTuples = map (map getScheduleFromString . (splitOn ",")) (lines contents)
+  let answerPart1 = foldr (\[x, y] -> (+ if isOverlapping x y then 1 else 0)) 0 assignmentTuples
+  print answerPart1
+
+  -- part 2
 
   -- tie up loose ends
   hClose handle
