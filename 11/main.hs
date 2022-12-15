50193ca25a553e57ce6c2d5b2e88854eb0960ebc
JT <jt@usekojo.com>
2022-12-14 16:04:13 -0800

    better operation parsing

--- 11/main.hs
+++ 11/main.hs
@@ -5,14 +5,30 @@ import Data.List.Split
 data Variable = DynamicValue | HardcodedValue Int deriving(Show)
 data OpType = Multiply | Add
 data Operation = Operation OpType Variable Variable
+data Expression = Expression OpType Int Int
 
-processOperation :: Operation -> Int -> Int
-processOperation (Operation Add x y) variable = 
+makeOperation :: String -> String -> String -> Operation
+makeOperation opStr str1 str2 = Operation opType var1 var2
+  where
+    opType = if opStr == "+" then Add else Multiply
+    var1 = if str1 == "old" then DynamicValue else HardcodedValue ((read str2)::Int)
+    var2 = if str2 == "old" then DynamicValue else HardcodedValue ((read str2)::Int)
+
+hydrateVariable :: Variable -> Int -> Int
+hydrateVariable (HardcodedValue hv) _ = hv
+hydrateVariable (DynamicValue) value = value
+
+convertOperationToExpression :: Operation -> Int -> Expression
+convertOperationToExpression (Operation opType var1 var2) value = Expression opType (hydrateVariable var1 value) (hydrateVariable var2 value)
+
+processExpression :: Expression -> Int
+processExpression (Expression Add x y) = x + y
+processExpression (Expression Multiply x y) = x * y
 
 data Monkey = Monkey {
   index :: Int,
   startingItems :: [Int],
-  operation :: Int -> Int,
+  operation :: Operation,
   test :: Int -> Bool,
   testTrue :: Int,
   testFalse :: Int
@@ -26,7 +42,10 @@ parseMonkey str = Monkey { index=index, startingItems=startingItems, operation=o
   where
     index = ((read :: String -> Int) . last . head) (str =~ "Monkey ([0-9]+):"::[[String]])
     startingItems = ((map (read :: String->Int)) . (splitOn ", ") . last . head) (str =~ "Starting items: (.*)"::[[String]])
-    operation = (+ 4)
+    opType = (last . head) (str =~ "Operation:.*([+*]).*"::[[String]])
+    var1 = (last . head) (str =~ "Operation:.*([a-z0-9]+) [+*].*"::[[String]])
+    var2 = (last . head) (str =~ "Operation:.*[+*] ([a-z0-9]+).*"::[[String]])
+    operation = makeOperation opType var1 var2
     divisible_by = ((read :: String->Int) . last . head) (str =~ "Test: divisible by ([0-9]+)"::[[String]])
     test = (== 0) . (`mod` divisible_by)
     testTrue = ((read :: String->Int) . last . head) (str =~ "If true: throw to monkey ([0-9]+)"::[[String]])
