8963070a1a83dc6c4add79bf756603aa9448b548
JT <jt@usekojo.com>
2022-12-15 20:25:30 -0800

    unify func in problem 6

--- 06/main.hs
+++ 06/main.hs
@@ -1,36 +1,26 @@
 import System.IO
 import qualified Data.List as L
 
-computeIndex :: String -> Int
-computeIndex [] = 0
-computeIndex str = computeIndexHelper 0 str
+computeIndex :: String -> Int -> Int
+computeIndex [] _ = 0
+computeIndex str num_uniq = computeIndexHelper 0 str num_uniq
   where
-    computeIndexHelper n [] = n
-    computeIndexHelper n (w:x:y:z:xs)
-      | length (L.nub([w, x, y, z])) == 4 = n + 4 -- L.nub removes duplicates
-      | otherwise = computeIndexHelper (n + 1) (x:y:z:xs)
-    computeIndexHelper n _ = 0
-
-computeIndexPart2 :: String -> Int
-computeIndexPart2 [] = 0
-computeIndexPart2 str = computeIndexHelperPart2 0 str
-  where
-    computeIndexHelperPart2 n [] = n
-    computeIndexHelperPart2 n list@(x:xs)
-      | length (L.nub(first_fourteen)) == 14 = n + 14
-      | otherwise = computeIndexHelperPart2 (n+1) xs
-      where first_fourteen = take 14 list
+    computeIndexHelper n [] _ = n
+    computeIndexHelper n list@(x:xs) num_uniq
+      | length (L.nub(first_n)) == num_uniq = n + num_uniq
+      | otherwise = computeIndexHelper (n+1) xs num_uniq
+      where first_n = take num_uniq list
 
 main = do
   handle <- openFile "/Users/josephbowler/agora/aoc2022/06/input.txt" ReadMode
   contents <- hGetContents handle
 
   -- part 1
-  let answerPart1 = computeIndex contents
+  let answerPart1 = computeIndex contents 4
   print answerPart1
 
   -- part 2
-  let answerPart2 = computeIndexPart2 contents
+  let answerPart2 = computeIndex contents 14
   print answerPart2
 
   -- clean up
