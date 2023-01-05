e53db3356330a370847dc36c78bb3585dcfd3994
JT <jt@usekojo.com>
2023-01-04 21:58:44 -0800

    refactor part 2

--- 02/main.hs
+++ 02/main.hs
@@ -1,35 +1,44 @@
 import System.IO
 
-computeSingleScore :: (String, String) -> Either String Int
-computeSingleScore (x,y)
-  | x == "A" && y == "X" = Right $ 1 + 3
-  | x == "A" && y == "Y" = Right $ 2 + 6
-  | x == "A" && y == "Z" = Right $ 3 + 0
-  | x == "B" && y == "X" = Right $ 1 + 0
-  | x == "B" && y == "Y" = Right $ 2 + 3
-  | x == "B" && y == "Z" = Right $ 3 + 6
-  | x == "C" && y == "X" = Right $ 1 + 6
-  | x == "C" && y == "Y" = Right $ 2 + 0
-  | x == "C" && y == "Z" = Right $ 3 + 3
-  | otherwise = Left $ "Invalid combo: (" ++ x ++ ", " ++ y ++ ")"
+data TurnAction = Rock | Paper | Scissors deriving(Show)
 
-computeSingleScorePart2 :: (String, String) -> Either String Int
-computeSingleScorePart2 (x,y)
-  | x == "A" && y == "X" = Right $ 3 + 0
-  | x == "A" && y == "Y" = Right $ 1 + 3
-  | x == "A" && y == "Z" = Right $ 2 + 6
-  | x == "B" && y == "X" = Right $ 1 + 0
-  | x == "B" && y == "Y" = Right $ 2 + 3
-  | x == "B" && y == "Z" = Right $ 3 + 6
-  | x == "C" && y == "X" = Right $ 2 + 0
-  | x == "C" && y == "Y" = Right $ 3 + 3
-  | x == "C" && y == "Z" = Right $ 1 + 6
-  | otherwise = Left $ "Invalid combo: (" ++ x ++ ", " ++ y ++ ")"
+parseTurnAction :: Char -> Either String TurnAction
+parseTurnAction 'A' = Right Rock
+parseTurnAction 'X' = Right Rock
+parseTurnAction 'B' = Right Paper
+parseTurnAction 'Y' = Right Paper
+parseTurnAction 'C' = Right Scissors
+parseTurnAction 'Z' = Right Scissors
+parseTurnAction str = Left $ "Not a proper turn action: " ++ [str]
 
-parseChoices :: String -> (String, String)
-parseChoices str = helper (words str)
-  where
-    helper [x,y] = (x,y)
+parseTurn :: String -> Either String (TurnAction, TurnAction)
+parseTurn [first_move_str, ' ', second_move_str] = do
+  first_turn_action <- parseTurnAction first_move_str
+  second_turn_action <- parseTurnAction second_move_str
+  Right (first_turn_action, second_turn_action)
+parseTurn t = Left $ "Error parsing turn: " ++ t
+
+computeSingleScorePart1 :: (TurnAction, TurnAction) -> Int
+computeSingleScorePart1 (Rock, Rock) = 1 + 3
+computeSingleScorePart1 (Rock, Paper) = 2 + 6
+computeSingleScorePart1 (Rock, Scissors) = 3 + 0
+computeSingleScorePart1 (Paper, Rock) = 1 + 0
+computeSingleScorePart1 (Paper, Paper) = 2 + 3
+computeSingleScorePart1 (Paper, Scissors) = 3 + 6
+computeSingleScorePart1 (Scissors, Rock) = 1 + 6
+computeSingleScorePart1 (Scissors, Paper) = 2 + 0
+computeSingleScorePart1 (Scissors, Scissors) = 3 + 3
+
+computeSingleScorePart2 :: (TurnAction, TurnAction) -> Int
+computeSingleScorePart2 (Rock, Rock) = 3 + 0
+computeSingleScorePart2 (Rock, Paper) = 1 + 3
+computeSingleScorePart2 (Rock, Scissors) = 2 + 6
+computeSingleScorePart2 (Paper, Rock) = 1 + 0
+computeSingleScorePart2 (Paper, Paper) = 2 + 3
+computeSingleScorePart2 (Paper, Scissors) = 3 + 6
+computeSingleScorePart2 (Scissors, Rock) = 2 + 0
+computeSingleScorePart2 (Scissors, Paper) = 3 + 3
+computeSingleScorePart2 (Scissors, Scissors) = 1 + 6
 
 main = do
   -- read file contents
@@ -38,18 +47,16 @@ main = do
 
   -- part 1
   let list = lines contents
-  let selections = map parseChoices list
+  let turns = mapM parseTurn list
 
-  let answerPart1 = mapM computeSingleScore selections
-  case answerPart1 of
-    Left error -> print $ "Error: " ++ error
-    Right answerPart1 -> print $ show (sum answerPart1)
-
-
-  -- part 2
-  let answerPart2 = mapM computeSingleScorePart2 selections
-  case answerPart2 of
-    Left error -> print $ "Error: " ++ error
-    Right answerPart2 -> print $ show (sum answerPart2)
+  case turns of
+    Right turns -> do 
+      -- part 1
+      let scoresPart1 = map computeSingleScorePart1 turns
+      print $ "Scores part 1: " ++ show (sum scoresPart1)
+      -- part 2
+      let scoresPart2 = map computeSingleScorePart2 turns
+      print $ "Scores part 2: " ++ show (sum scoresPart2)
+    Left error -> print "Couldn't parse turns for part 1"
 
   hClose handle
